dfe4060aed13ac3a5f1593293596c8c8fec32d63
Suyash Joshi <suyashjoshi088@gmail.com>
2025-02-24 22:19:51 -0500

    linkedlist java from scratch

---
+++ DSA/src/LinkedList/SinglyLinkedList/Main.java
@@ -0,0 +1,15 @@
+package LinkedList.SinglyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkList101 list = new LinkList101();
+
+        list.insertFirst(3);
+        list.insertFirst(4);
+        list.insertFirst(5);
+        list.insertFirst(6);
+        list.insertFirst(7);
+
+        list.display();
+    }
+}
